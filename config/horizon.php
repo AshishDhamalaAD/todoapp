2e88e37ec053a5da6d453d3f1c9ce355c88ea755
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2025-05-23 17:22:18 +0100

    add staging environment on the horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -198,17 +198,13 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
-                'balanceMaxShift' => 1,
-                'balanceCooldown' => 3,
+                'maxProcesses' => 3,
             ],
         ],
 
         'staging' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
-                'balanceMaxShift' => 1,
-                'balanceCooldown' => 3,
+                'maxProcesses' => 3,
             ],
         ],
 
